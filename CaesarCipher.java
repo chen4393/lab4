33a18961e25b9be93e16736db5c8ec2ae3e663c7
Chaoran Chen <chaoranchen@Chaorans-MacBook-Pro.local>
2017-02-16 01:41:05 -0600

    add q3

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CaesarCipher {
 	private int key;
 	public CaesarCipher(int key) {	this.key = key;}
@@ -24,10 +26,46 @@ public class CaesarCipher {
 		}
 		return sb.toString();
 	}
+	public static void crack(String message) {
+		StringBuilder sb = new StringBuilder(message);
+		for (int i = 0; i < 26; i++) {
+			CaesarCipher cc = new CaesarCipher(i);
+			System.out.println(cc.decrypt(message) + " - key = " + i);
+		}
+	}
 	public static void main(String[] args) {
-		CaesarCipher cc = new CaesarCipher(3);
-		String cipher = cc.encrypt("xyz XYZ abc ABC");
-		System.out.println(cipher);
-		System.out.println(cc.decrypt(cipher));
+		Scanner sc = new Scanner(System.in);
+		System.out.print("What is your key? ");
+		int key = 0;
+		if (sc.hasNext())
+			key = sc.nextInt();
+		CaesarCipher cc = new CaesarCipher(key);
+		while (true) {
+			sc = new Scanner(System.in);
+			String cmd = "";
+			System.out.println("What would you like to do?\n"
+			+ "encypt\n" + "decrypt\n" + "crack\n" + "quit\n");
+			if (sc.hasNext())
+				cmd = sc.nextLine();
+			sc = new Scanner(System.in);
+			if (cmd.equals("encrypt")) {
+				System.out.print("Please enter the message ");
+				String msg = sc.nextLine();
+				System.out.println(cc.encrypt(msg));
+			}
+			else if (cmd.equals("decrypt")) {
+				System.out.print("Please enter the message ");
+				String msg = sc.nextLine();
+				System.out.println(cc.decrypt(msg));
+			}
+			else if (cmd.equals("crack")) {
+				System.out.print("Please enter the message ");
+				String msg = sc.nextLine();
+				crack("Pe ef Mcfep");
+			}
+			else if (cmd.equals("quit"))
+				break;
+			System.out.println();
+		}
 	}
 }
