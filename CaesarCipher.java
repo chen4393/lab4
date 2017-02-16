9509b970cc7ffb041f5c90cc5013a232f45f9413
Chaoran Chen <chaoranchen@x-10-104-73-21.uofm-secure.wireless.umn.edu>
2017-02-16 17:44:06 -0600

    final graded

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 
 public class CaesarCipher {
 	private int key;
-	public CaesarCipher(int key) {	this.key = key;}
+	private int[] alphabet;
+	public CaesarCipher(int key) {	
+		this.key = key;
+		alphabet = new int[26];
+	}
 	public String encrypt(String message) {
 		StringBuilder sb = new StringBuilder(message);
 		for (int i = 0; i < message.length(); i++) {
 			if (sb.charAt(i) == ' ')	continue;
 			int temp = (int)sb.charAt(i);
 			int origin = (temp >= 97) ? 97 : 65;
+			alphabet[temp - origin]++;
 			temp = origin + (temp - origin + key) % 26;
 			sb.setCharAt(i, (char)temp);
 		}
@@ -31,6 +36,11 @@ public class CaesarCipher {
 		for (int i = 0; i < 26; i++) {
 			CaesarCipher cc = new CaesarCipher(i);
 			System.out.println(cc.decrypt(message) + " - key = " + i);
+		}
+	}
+	public void displayEncryptionStats() {
+		for (int i = 0; i < alphabet.length; i++) {
+			System.out.println((char)(97 + i) + " has been encrypted " + alphabet[i] + " times");
 		}
 	}
 	public static void main(String[] args) {
@@ -63,8 +73,10 @@ public class CaesarCipher {
 				String msg = sc.nextLine();
 				crack("Pe ef Mcfep");
 			}
-			else if (cmd.equals("quit"))
+			else if (cmd.equals("quit")) {
+				cc.displayEncryptionStats();
 				break;
+			}
 			System.out.println();
 		}
 	}
